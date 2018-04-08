ed847ebd3c338ade5386bf4577fcb191064d4219
Zwb <ethercflow@gmail.com>
2018-04-08 14:29:26 +0800

    move fcitx to modules/fcitx

---
+++ modules/tools/fcitx/config.el
@@ -0,0 +1,9 @@
+;;; tools/fcitx/config.el --- description -*- lexical-binding: t; -*-
+
+(def-package! fcitx
+  :after evil
+  :config
+  (fcitx-evil-turn-on))
+
+(provide 'config)
+;;; tools/fcitx/config.el ends here
