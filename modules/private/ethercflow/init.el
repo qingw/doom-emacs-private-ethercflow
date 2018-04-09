4c3a067552171a7c888e8f0372bf0baefcfea861
Zwb <ethercflow@gmail.com>
2018-04-09 21:19:06 +0800

    set more font

--- modules/private/ethercflow/init.el
+++ modules/private/ethercflow/init.el
@@ -5,4 +5,9 @@
 
 (setq default-frame-alist '((ns-transparent-titlebar . t) (ns-appearance . 'nil)))
 
-(setq doom-font (font-spec :family "Inconsolata" :size 17))
+(setq doom-font (font-spec :family "Inconsolata" :size 17)
+      doom-big-font (font-spec :family "Inconsolata" :size 17)
+      doom-unicode-font (font-spec :family "Inconsolata" :size 17)
+      doom-variable-pitch-font (font-spec :family "Inconsolata" :size 17))
+
+(global-whitespace-mode)
