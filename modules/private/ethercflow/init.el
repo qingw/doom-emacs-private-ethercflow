54c5ce19e24d43ad5b221798474ea58608b797ec
Zwb <ethercflow@gmail.com>
2018-04-09 21:05:39 +0800

    set font

--- modules/private/ethercflow/init.el
+++ modules/private/ethercflow/init.el
@@ -2,3 +2,7 @@
 
 (setq user-mail-address "ethercflow@gmail.com"
       user-full-name    "Zwb")
+
+(setq default-frame-alist '((ns-transparent-titlebar . t) (ns-appearance . 'nil)))
+
+(setq doom-font (font-spec :family "Inconsolata" :size 17))
