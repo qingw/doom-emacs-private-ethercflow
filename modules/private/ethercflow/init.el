9e33b5a4832ec13b1aa68c29df4d3963787f0934
Zwb <ethercflow@gmail.com>
2018-04-13 22:10:31 +0800

    call whitespace-cleanup before save buffer

--- modules/private/ethercflow/init.el
+++ modules/private/ethercflow/init.el
@@ -9,3 +9,5 @@
       doom-big-font (font-spec :family "Inconsolata" :size 17)
       doom-unicode-font (font-spec :family "Inconsolata" :size 17)
       doom-variable-pitch-font (font-spec :family "Inconsolata" :size 17))
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
