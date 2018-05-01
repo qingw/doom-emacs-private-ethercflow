47b9aebe30fff1d2521079a6aa03986c289299ba
Zwb <ethercflow@gmail.com>
2018-05-01 22:14:19 +0800

    add systemtap layer

---
+++ modules/lang/systemtap/config.el
@@ -0,0 +1,3 @@
+;;; lang/systemtap/config.el -*- lexical-binding: t; -*-
+
+(def-package! systemtap-mode)
---
+++ modules/lang/systemtap/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; lang/systemtap/packages.el
+
+(package! systemtap-mode :recipe (:fetcher github :repo "ethercflow/systemtap-mode"))
