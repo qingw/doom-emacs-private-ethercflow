62cb7c7862e22709aebf6d7acd6f303729298b6b
Zwb <ethercflow@gmail.com>
2018-05-04 23:46:52 +0800

    fix fonts bug

--- modules/tools/fonts/config.el
+++ modules/tools/fonts/config.el
@@ -2,6 +2,8 @@
 
 (def-package! cnfonts
   :init
+  (add-hook 'after-init-hook #'cnfonts-enable)
+  :config
   (setq cnfonts-use-face-font-rescale t)
   (setq cnfonts-directory "~/.config/doom/modules/tools/fonts/profiles")
-  (cnfonts-enable))
+  )
