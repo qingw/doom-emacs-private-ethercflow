c3f85eb7be8bf0a5771d824a5b680598504da9ad
Zwb <ethercflow@gmail.com>
2018-05-04 18:10:21 +0800

    rename font to fonts

--- init.el
+++ init.el
@@ -118,7 +118,7 @@
 
        :tools
        org-page          ; a blog generator
-       cnfonts           ; chinese-fonts-setup
+       fonts             ; chinese-fonts-setup
 
        :config
        ;; The default module set reasonable defaults for Emacs. It also provides
