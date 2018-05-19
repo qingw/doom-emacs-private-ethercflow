3e3f00f2aa31b0ae6208422fae9ce3c7c887b3e6
Zwb <ethercflow@gmail.com>
2018-05-19 19:30:06 +0800

    add private python setting

---
+++ modules/lang/ppython/config.el
@@ -0,0 +1,18 @@
+;; -*- no-byte-compile: t; -*-
+;;; lang/ppython/config.el
+
+(def-package! pyenv-mode
+  :when (featurep! :lang python)
+  :config
+  (setenv "WORKON_HOME" "~/anaconda/envs")
+  (pyenv-mode 1)
+
+  (defun projectile-pyenv-mode-set ()
+    "Set pyenv version matching project name."
+    (let ((project (projectile-project-name)))
+      (if (member project (pyenv-mode-versions))
+          (pyenv-mode-set project)
+        (pyenv-mode-unset))))
+
+  (add-hook 'projectile-after-switch-project-hook
+            'projectile-pyenv-mode-set))
---
+++ modules/lang/ppython/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; lang/ppython/packages.el
+
+(package! pyenv-mode)
